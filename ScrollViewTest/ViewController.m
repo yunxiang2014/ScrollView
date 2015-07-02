35f83e7500de6268dc1d36da9f0a02db389ee43f
charles <ychai@my.okcu.edu>
2015-07-02 17:26:33 -0500

    IOS_ScrollView

--- ScrollViewTest/ViewController.m
+++ ScrollViewTest/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 
 @end
 
@@ -17,11 +18,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    CGFloat with = 400;
+    CGFloat high = 600;
+    
+    for (int i; i< 6; i++) {
+        UIImageView *imgae = [[UIImageView alloc]init];
+        CGFloat imageX = (i-1) * with;
+        CGFloat imageY = 0.f;
+        imgae.frame = CGRectMake(imageX, imageY, with, high);
+        imgae.image = [UIImage imageNamed:[NSString stringWithFormat:@"img_%02d",i]];
+        
+        [self.scrollView addSubview:imgae];
+    }
+    
+    self.scrollView.contentSize = CGSizeMake(5 * with, 0);
+    
+    self.scrollView.pagingEnabled = YES;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
